12c5b1402bc67949ca39230434e190398efc5566
ArkadiusDS <arcadius.modzelewski@gmail.com>
2024-07-08 15:59:45 +0200

    Add search method

--- search.py
+++ search.py
@@ -5,7 +5,6 @@ from pprint import pprint
 from dotenv import load_dotenv
 from elasticsearch import Elasticsearch
 
-
 load_dotenv()
 
 
@@ -58,7 +57,6 @@ class Search:
             operations.append(document)
         return self.es.bulk(operations=operations)
 
-
     def reindex(self):
         """
         Recreates the 'my_documents' index and inserts documents from 'data.json'.
@@ -73,3 +71,20 @@ class Search:
             documents = json.loads(f.read())
         return self.insert_documents(documents)
 
+    def search(self, **query_args):
+        """
+        Searches for documents in the 'my_documents' index using the provided query arguments.
+
+        Parameters
+        ----------
+        **query_args : dict
+            Arbitrary keyword arguments to be passed as the search query parameters.
+            These can include various Elasticsearch search options like 'query',
+            'from', 'size', etc.
+
+        Returns
+        -------
+        dict
+            The response from the search operation, containing search hits and metadata.
+        """
+        return self.es.search(index='my_documents', **query_args)
